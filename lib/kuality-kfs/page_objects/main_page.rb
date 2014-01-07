f49c1d9bec594d9d5e63f92cdef43ddce2ab1776
Tony <tony@rsmart.com>
2014-01-07 08:19:11 -0500

    account delegate from and to amounts added [KFSQA-567]

--- lib/kuality-kfs/page_objects/main_page.rb
+++ lib/kuality-kfs/page_objects/main_page.rb
@@ -5,6 +5,7 @@ class MainPage < BasePage
   #TODO abe to talk about links and button helper!
 
   action(:account) { |b| b.frm.link(text: 'Account').click }
+  action(:account_delegate) { |b| b.frm.link(text: 'Account Delegate').click }
   action(:account_global) { |b| b.frm.link(text: 'Account Global').click }
 
 end
