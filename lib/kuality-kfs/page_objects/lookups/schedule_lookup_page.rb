561d4a137b40beffe445649d3ef36b9d9ddbcc9a
Tony <tony@rsmart.com>
2014-01-08 11:34:04 -0500

    refactoring/merge problem fixed

--- lib/kuality-kfs/page_objects/lookups/schedule_lookup_page.rb
+++ lib/kuality-kfs/page_objects/lookups/schedule_lookup_page.rb
@@ -6,7 +6,5 @@ class ScheduleLookupPage < Lookups
   element(:status) { |b| b.frm.select(name: 'status') }
 
   element(:modify_links) { |b| b.results_table.links(text: 'modify') }
-  element(:edit_value_links) { |b| b.results_table.links(text: 'edit') }
-  element(:copy_value_links) { |b| b.results_table.links(text: 'copy') }
 
 end
