b4b37fb2ab5dd56ab311502acd625a786546e7a1
Todd Plesniak <tplesniak@rsmart.com>
2014-01-09 13:04:16 -0500

    adding sub object code global pages

---
+++ lib/kuality-kfs/data_objects/chart_of_accounts/sub_object_code_global_object.rb
@@ -0,0 +1,77 @@
+class SubObjectCodeGlobalObject < DataObject
+
+#  include Navigation
+#  include DateFactory
+  include StringFactory
+
+  attr_accessor  :description, :new_chart_code,
+                 :organization_document_number,
+                 :new_fiscal_year, :new_chart_code, :new_sub_object_code, :new_sub_object_code_name, :new_sub_object_code_short_name,
+                 :noc_fiscal_year, :noc_chart_code, :noc_object_code,
+                 :na_chart_code, :na_account_number,
+                 :multiple_accounting_lines_account_number_lookup
+
+
+
+  def   initialize(browser, opts={})
+    @browser = browser
+
+    defaults = {
+        description:                   random_alphanums(40, 'AFT'),
+        new_chart_code:               'IT - Ithaca Campus', #TODO grab this from config file
+        organization_document_number: '1000710', #TODO get from config
+        new_fiscal_year:              '2014',
+        new_chart_code:               'IT - Ithaca Campus', #TODO grab this from config file
+        new_sub_object_code:          'tst',
+        new_sub_object_code_name:     random_alphanums(20, 'AFT'),
+        new_sub_object_code_short_name: random_alphanums(5, 'ATF'),
+        noc_fiscal_year:              '2014',
+        noc_chart_code:               'IT - Ithaca Campus', #TODO grab this from config file
+        noc_object_code:              '1000',
+        na_chart_code:                'IT - Ithaca Campus', #TODO grab this from config file
+        na_account_number:            '1000710',
+        multiple_accounting_lines_account_number_lookup:        '100071*'
+
+
+    }
+    set_options(defaults.merge(opts))
+  end
+
+
+  def create
+    visit(MainPage).sub_object_code_global
+    on SubObjectCodeGlobalPage do |page|
+
+      #page.description.focus
+      #page.alert.ok if page.alert.exists? # Because, y'know, sometimes it doesn't actually come up...
+      #
+       fill_out page,  :description, :new_chart_code,
+                :organization_document_number,
+                :new_fiscal_year, :new_chart_code, :new_sub_object_code, :new_sub_object_code_name, :new_sub_object_code_short_name,
+                :noc_fiscal_year, :noc_chart_code, :noc_object_code,
+                :na_chart_code, :na_account_number
+
+       add_multiple_account_lines
+      page.save
+      @document_id = page.document_id
+
+    end
+
+  end
+
+  def add_multiple_account_lines
+    if !@multiple_accounting_lines_account_number_lookup.nil?
+    on(SubObjectCodeGlobalPage).add_multiple_account_lines
+
+      on AccountLookupPage do |page|
+        page.number.fit  @multiple_accounting_lines_account_number_lookup
+        page.search
+        page.select_all_from_this_page
+        page.return_selected
+      end
+    end
+  end
+
+
+
+end #class
