fa5050217babb2e74dcedb8cfad6487e98dc1db7
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-01-09 11:43:52 -0500

    [KFSQA-618] Made some style updates and fixed the account global object and step defs so it will run. Removed extraneous @wip tags. Now we just need to split Cornell specific stuff out of the kuality-kfs project.

--- lib/kuality-kfs/data_objects/chart_of_accounts/account_global_object.rb
+++ lib/kuality-kfs/data_objects/chart_of_accounts/account_global_object.rb
@@ -4,22 +4,18 @@ class AccountGlobalObject < DataObject
 #  include DateFactory
   include StringFactory
 
-
-  attr_accessor :cornell_university,
-                :description, :fo_principal_name, :supervisor_principal_name,
+  attr_accessor :description, :fo_principal_name, :supervisor_principal_name,
                 :org_cd, :sub_fnd_group_code, :acct_expire_date,
-                :postal_cd, :city, :state, :address,
+                :postal_code, :city, :state, :address,
                 :contintuation_coa_code, :contintuation_acct_number, :income_stream_financial_cost_cd, :income_stream_account_number,
                 :cfda_number,  :higher_ed_funct_cd, :sufficient_funds_cd,
                 :trans_processing_sufficient_funds_code, :labor_benefit_rate_category_code,
-                :new_chart_code, :new_number
+                :new_chart_code, :new_number, :major_reporting_category_code
 
   def initialize(browser, opts={})
     @browser = browser
 
     defaults = {
-        cornell_university: 'yes',
-
         description:          random_alphanums(20, 'AFT'),
         new_chart_code:           'IT - Ithaca Campus', #TODO grab this from config file
         new_number:               '1000710', #TODO get from config
@@ -28,7 +24,7 @@ class AccountGlobalObject < DataObject
         org_cd:               'BI',
         sub_fnd_group_code:   '',
         acct_expire_date:     '',
-        postal_cd:            '14853',
+        postal_code:            '14853',
         city:                 'Ithaca',
         state:                'NY',
         address:              'Cornell University',
@@ -41,6 +37,9 @@ class AccountGlobalObject < DataObject
         sufficient_funds_cd:    'C - Consolidation',
         trans_processing_sufficient_funds_code: '',
         labor_benefit_rate_category_code: ''
+
+        #organization_code: '00*'
+
     }
     set_options(defaults.merge(opts))
   end
@@ -50,10 +49,16 @@ class AccountGlobalObject < DataObject
     on AccountGlobalPage do |page|
       page.description.focus
       page.alert.ok if page.alert.exists? # Because, y'know, sometimes it doesn't actually come up...
-      fill_out page, :description, :new_chart_code, :new_number
+      fill_out page, :description, :fo_principal_name, :supervisor_principal_name,
+               :org_cd, :sub_fnd_group_code, :acct_expire_date,
+               :postal_code, :city, :state, :address,
+               :contintuation_coa_code, :contintuation_acct_number, :income_stream_financial_cost_cd, :income_stream_account_number,
+               :cfda_number,  :higher_ed_funct_cd, :sufficient_funds_cd,
+               :trans_processing_sufficient_funds_code, :labor_benefit_rate_category_code,
+               :new_chart_code, :new_number, :major_reporting_category_code
       page.add_account_detail
 
-      createCornellAccountGlobalObject  if cornell_university == 'yes'
+      add_multiple_accounting_lines
 
       page.save
       @document_id = page.document_id
@@ -68,10 +73,15 @@ class AccountGlobalObject < DataObject
     on(AccountGlobalPage).submit
   end
 
-  #TODO:: Move this to CU file and edit create.
-  def createCornellAccountGlobalObject
+  def add_multiple_accounting_lines
+    if !@add_multiple_accounting_lines.nil?
       on AccountGlobalPage do |page|
-        fill_out page, :major_reporting_category_code
+        page.add_multiple_accounting_lines
+        page.account_number.fit search_account_number
+        page.search
+        page.select_all_rows_from_this_page
+        page.return_selected_results
       end
+    end
   end
 end
