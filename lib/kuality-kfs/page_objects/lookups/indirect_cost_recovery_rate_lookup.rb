c0d94246710b5e3e1597a7c442a80a5dcf2a85dd
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-01-10 13:49:36 -0500

    [KFSQA-576] Moved indirect_cost_recovery_rate_lookup.rb to the lookups directory.

--- lib/kuality-kfs/page_objects/lookups/indirect_cost_recovery_rate_lookup.rb
+++ lib/kuality-kfs/page_objects/lookups/indirect_cost_recovery_rate_lookup.rb
@@ -1,11 +1,9 @@
-class IndirectCostRecoveryRateLookupPage < BasePage
-
-  tiny_buttons
-  search_results_table
+class IndirectCostRecoveryRateLookupPage < Lookups
 
   element(:university_fiscal_year) { |b| b.frm.text_field(name: 'universityFiscalYear') }
   element(:rate_id) { |b| b.frm.text_field(name: 'financialIcrSeriesIdentifier') }
   action(:active_indicator_yes) { |b| b.frm.radio(id: 'activeYes').set }
   action(:active_indicator_no) { |b| b.frm.radio(id: 'activeNo').set }
   action(:active_indicator_both) { |b| b.frm.radio(id: 'activeBoth').set }
+
 end
