a0a5c986bdcbf9c6e7cb2fdd424c6b4c859f4429
Todd Plesniak <tplesniak@rsmart.com>
2014-01-21 09:47:40 -0500

    [KFSQA-571] made some changes to input data because it was failing the test.
    Please review changes to defaults, Also ‘Super’ cornel file is not in
    this branch but should work when merged with master.

--- lib/kuality-kfs/data_objects/chart_of_accounts/account_global_object.rb
+++ lib/kuality-kfs/data_objects/chart_of_accounts/account_global_object.rb
@@ -26,9 +26,9 @@ class AccountGlobalObject < AccountGlobalExtendedAttributesObject
         description:          random_alphanums(20, 'AFT'),
         new_chart_code:           'IT - Ithaca Campus', #TODO grab this from config file
         new_number:               '1000710', #TODO get from config
-        supervisor_principal_name:  'jaraujo',
+        supervisor_principal_name:  'dh273', #'jaraujo',
         manager_principal_name: 'warriaga',
-        org_cd:               'BI',
+        org_cd:              '01F9',# 'BI',
         sub_fnd_group_code:   '',
         acct_expire_date:     '',
         postal_code:            '14853',
@@ -38,7 +38,7 @@ class AccountGlobalObject < AccountGlobalExtendedAttributesObject
         contintuation_coa_code: '',
         contintuation_acct_number: '',
         income_stream_financial_cost_cd:  'IT - Ithaca Campus',
-        income_stream_account_number:     '0142900',
+        income_stream_account_number:    '1400831',# '0142900',
         cfda_number:          '',
         higher_ed_funct_cd:   '',
         sufficient_funds_cd:    'C - Consolidation',
