3cf232f1f028c47ed95de9d5d97a8f7f343fd0b4
Todd Plesniak <tplesniak@rsmart.com>
2014-01-27 15:05:44 -0700

    master merge completed with branch KFSQA-623

--- lib/kuality-kfs/data_objects/financial_processing/budget_adjustment_object.rb
+++ lib/kuality-kfs/data_objects/financial_processing/budget_adjustment_object.rb
@@ -3,23 +3,19 @@ class BudgetAdjustmentObject < KFSDataObject
   attr_accessor  :fdd_year, :from_chart_code, :from_account_number, :from_object_code, :from_current_amount,
                  :to_chart_code, :to_object_code, :to_current_amount
 
-      def initialize(browser, opts={})
+  def initialize(browser, opts={})
     @browser = browser
 
-    defaults = {
-        description:    random_alphanums(20, 'AFT'),
+    defaults = { description:    random_alphanums(20, 'AFT'),
         from_chart_code: 'IT',
         from_account_number: ['1258322'],
         from_object_code: '4480',
         from_current_amount: '10000'
-
     }
     set_options(defaults.merge(opts))
   end
 
-  def create
-    pre_create
-
+  def build
     visit(MainPage).budget_adjustment
 
     on BudgetAdjustmentPage do |page|
@@ -38,22 +34,11 @@ class BudgetAdjustmentObject < KFSDataObject
 
       page.send(@press) unless @press.nil?
     end
-
-    post_create
   end
 
-  def save
-    on(BugdetAdjustmentPage).save
-  end
-
-  def submit
-    on(BugdetAdjustmentPage).submit
-  end
-
-  def view
-    #@browser.goto "#{$base_url}kr/maintenance.do?methodToCall=docHandler&docId=#{@document_id}&command=displayDocSearchView"
+  def viewAsUse
     #TODO:: Fix this url
-    @browser.goto "#{base_url}channelTitle=Budget%20Adjustment&channelUrl=financialBudgetAdjustment.do?methodToCall=docHandler&command=initiate&docTypeName=BA&backdoorId=dh273"
+    @browser.goto "#{base_url}channelTitle=Budget%20Adjustment&channelUrl=financialBudgetAdjustment.do?methodToCall=docHandler&command=initiate&docTypeName=BA&backdoorId=#{$users.current_user}"
   end
 
 end #class
