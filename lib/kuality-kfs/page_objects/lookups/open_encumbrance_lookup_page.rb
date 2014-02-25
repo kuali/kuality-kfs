f7143ecbd7766ab00db7d64d7af36c1a570fbf52
Tony <tony@rsmart.com>
2014-02-24 17:01:58 -0700

    Encumbrance lookup [KFSQA-654]

--- lib/kuality-kfs/page_objects/lookups/open_encumbrance_lookup_page.rb
+++ lib/kuality-kfs/page_objects/lookups/open_encumbrance_lookup_page.rb
@@ -6,4 +6,10 @@ class OpenEncumbranceLookupPage < Lookups
   fiscal_year_facets
   encumbrance_object_facets
 
+  element(:balance_type_code) { |b| b.frm.text_field(id: 'balanceTypeCode') }
+
+  element(:active_indicator_no) { |b| b.frm.radio(id: 'dummyBusinessObject.pendingEntryOptionNo') }
+  element(:active_indicator_approved) { |b| b.frm.radio(id: 'dummyBusinessObject.pendingEntryOptionApproved') }
+  element(:active_indicator_all) { |b| b.frm.radio(id: 'dummyBusinessObject.pendingEntryOptionAll') }
+
 end
