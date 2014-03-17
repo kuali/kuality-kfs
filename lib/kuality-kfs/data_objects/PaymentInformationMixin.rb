75951cc98d9cb523af628ae53488df3e9b4237a5
lshyu <lshyu@vivantech.com>
2014-03-17 14:00:38 -0400

    [KFSQA-700] scripts to test that DV allows petty cash and check amount change

--- lib/kuality-kfs/data_objects/PaymentInformationMixin.rb
+++ lib/kuality-kfs/data_objects/PaymentInformationMixin.rb
@@ -73,7 +73,7 @@ module PaymentInformationMixin
       plookup.search
       plookup.return_value(@payee_id)
     end
-    if vendor_payee?
+    if vendor_payee? && !@address_type_description.nil?
       on VendorAddressLookup do |valookup|
         valookup.address_1.fit @address_1 unless @address_1.nil?
         valookup.address_2.fit @address_2 unless @address_2.nil?
