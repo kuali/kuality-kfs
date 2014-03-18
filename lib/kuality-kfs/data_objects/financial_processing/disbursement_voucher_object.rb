7243cfc0104d3c9f30d719eccf69b9d4b4256815
lshyu <lshyu@vivantech.com>
2014-03-18 10:24:07 -0400

    [KFSQA-709] more files for merging master to this branch

--- lib/kuality-kfs/data_objects/financial_processing/disbursement_voucher_object.rb
+++ lib/kuality-kfs/data_objects/financial_processing/disbursement_voucher_object.rb
@@ -1,10 +1,10 @@
 class DisbursementVoucherObject < KFSDataObject
 
-  DOC_INFO = { label: 'Disbursement Voucher Document', type_code: 'DV' }
-
   include PaymentInformationMixin
   include AccountingLinesMixin
   alias :add_target_line :add_source_line
+
+  DOC_INFO = { label: 'Disbursement Voucher Document', type_code: 'DV' }
 
   attr_accessor :organization_document_number, :explanation,
                 :contact_name, :phone_number, :email_address,
@@ -14,11 +14,11 @@ class DisbursementVoucherObject < KFSDataObject
     @browser = browser
 
     defaults = {
-                 description:                       random_alphanums(40, 'AFT'),
-                 #foreign_draft_in_foreign_currency: :set,
-                 #currency_type:                     'Canadian $'
-               }.merge!(default_accounting_lines)
-                .merge!(default_payment_information_lines)
+        description:                       random_alphanums(40, 'AFT'),
+        #foreign_draft_in_foreign_currency: :set,
+        #currency_type:                     'Canadian $'
+    }.merge!(default_accounting_lines)
+     .merge!(default_payment_information_lines)
 
     set_options(defaults.merge(opts))
   end
@@ -32,16 +32,6 @@ class DisbursementVoucherObject < KFSDataObject
       fill_out page, :description, :organization_document_number, :explanation,
                      :contact_name, :phone_number, :email_address,
                      :foreign_draft_in_usd, :foreign_draft_in_foreign_currency, :currency_type
-    end
-  end
-
-  def view
-    visit(MainPage).doc_search
-    on DocumentSearch do |search|
-      search.document_type.fit ''
-      search.document_id.fit @document_id
-      search.search
-      search.open_doc @document_id
     end
   end
 
