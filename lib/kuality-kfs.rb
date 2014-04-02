23fd1a3666d079d7e3dbdcdd09912d6940b24654
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-04-02 17:32:05 -0400

    Added in a bit of code that should make running parallel tests with number of browsers greater than number of CPUs more reliable.

--- lib/kuality-kfs.rb
+++ lib/kuality-kfs.rb
@@ -22,7 +22,19 @@ class KualityKFS
   attr_reader :browser
 
   def initialize(web_browser)
-    @browser = Watir::Browser.new web_browser
+    unless (env_no = ENV['TEST_ENV_NUMBER'].to_i).zero?
+      # As described in the readme
+      profile = Selenium::WebDriver::Firefox::Profile.new
+      profile['webdriver.firefox.port'] = (8888 + env_no);
+      @browser = Watir::Browser.new web_browser, :profile => profile
+
+      # Enforces a sleep time, i need to multiply by 10 to achieve consistent results on
+      # my 8 cores vm, may work for less though.
+      sleep env_no * 10
+    else
+      @browser = Watir::Browser.new web_browser
+    end
+
     @browser.window.resize_to(1400,900)
     @browser.goto $base_url
   end
