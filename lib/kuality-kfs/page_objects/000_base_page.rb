1ff870d910d31e3be51a4ccd59642c0aacf926f1
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-04-16 17:15:30 -0400

    [KFSQA-838] Refactored some steps that have to do with routing into their own step definition file. Worked on some steps, though nothing has been tested yet.

--- lib/kuality-kfs/page_objects/000_base_page.rb
+++ lib/kuality-kfs/page_objects/000_base_page.rb
@@ -168,6 +168,10 @@ class BasePage < PageFactory
       element(:actions_taken_table) { |b| b.route_log_iframe.div(id: 'tab-ActionsTaken-div').table }
       value(:actions_taken) { |b| (b.actions_taken_table.rows.collect{ |row| row[1].text }.compact.uniq).reject{ |action| action==''} }
       element(:pnd_act_req_table) { |b| b.route_log_iframe.div(id: 'tab-PendingActionRequests-div').table }
+      value(:pnd_act_req_table_action) { |r=1, b| b.pnd_act_req_table[r].tds[b.pnd_act_req_table.keyed_column_index(:action)] }
+      value(:pnd_act_req_table_requested_of) { |r=1, b| b.pnd_act_req_table[r].tds[b.pnd_act_req_table.keyed_column_index(:requested_of)] }
+      value(:pnd_act_req_table_time_date) { |r=1, b| b.pnd_act_req_table[r].tds[b.pnd_act_req_table.keyed_column_index(:time_date)] }
+      value(:pnd_act_req_table_annotation) { |r=1, b| b.pnd_act_req_table[r].tds[b.pnd_act_req_table.keyed_column_index(:annotation)] }
       value(:action_requests) { |b| (b.pnd_act_req_table.rows.collect{ |row| row[1].text}).reject{ |action| action==''} }
       action(:show_future_action_requests) { |b| b.route_log_iframe.h2(text: 'Future Action Requests').parent.parent.image(title: 'show').click }
       element(:future_actions_table) { |b| b.route_log_iframe.div(id: 'tab-FutureActionRequests-div').table }
