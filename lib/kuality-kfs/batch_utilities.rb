556bd7d5ffadc25495e3bf65e3443714794acecb
lshyu <lshyu@vivantech.com>
2014-04-21 07:58:30 -0400

    [KFSQA-763]  scripts to test REQS creation/CM assignment, PO submit, PREQ creation/submit.  Also added to run autoApprovePreq batch after PREQ is submitted.

--- lib/kuality-kfs/batch_utilities.rb
+++ lib/kuality-kfs/batch_utilities.rb
@@ -18,14 +18,21 @@ module BatchUtilities
       page.run_job
       if wait_for_completion == true
         x = 0
-        while x < 12 # Hell, let's give it a whole minute if it needs it.
-          break if (page.job_status =~ /Succeeded/) == 0
-          sleep 5
+        while page.job_status.include?('Running') # Hell, let's give it a whole minute if it needs it.
+          #break if (page.job_status =~ /Succeeded/) == 0
+          sleep 30
           page.refresh
-          x += 1
         end
       end
     end
+  end
+
+  def run_auto_approve_preq(wait_for_completion = false)
+    run_unscheduled_job('autoApprovePaymentRequestsJob', wait_for_completion)
+  end
+
+  def run_fax_pending_doc(wait_for_completion = false)
+    run_unscheduled_job('faxPendingDocumentJob', wait_for_completion)
   end
 
 end
