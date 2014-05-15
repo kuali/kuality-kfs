a039559e38b08a233701794f06802135b0fe1f4c
lshyu <lshyu@vivantech.com>
2014-05-15 16:32:15 -0400

    [KFSQA-974] use today's month

--- lib/kuality-kfs/page_objects/lookups/001_ledger_balance_lookup_page.rb
+++ lib/kuality-kfs/page_objects/lookups/001_ledger_balance_lookup_page.rb
@@ -1,7 +1,7 @@
 class LedgerBalanceLookupPage < Lookups
   fiscal_year_facets
   element(:monthly_boxes) { |b| b.frm.checkboxes(name: /^selectedObjId/m) }
-  element(:month_box) { |month='July', b| b.frm.checkbox(name: /^selectedObjId/m, title: "#{month}") }
+  element(:month_box) { |month="#{Date::MONTHNAMES[Date.today.month]}", b| b.frm.checkbox(name: /^selectedObjId/m, title: "#{month}") }
 
   element(:check_first_month) do |b|
     b.monthly_boxes.each do |cb|
