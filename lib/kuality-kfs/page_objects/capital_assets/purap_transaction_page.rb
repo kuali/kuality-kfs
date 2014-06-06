c73b827deb7dca271cdafe7a90d3d8240e6a71cc
lshyu <lshyu@vivantech.com>
2014-06-06 09:45:50 -0400

    [KFSQA-994] testing for debugging locate element on puraptransaction page

--- lib/kuality-kfs/page_objects/capital_assets/purap_transaction_page.rb
+++ lib/kuality-kfs/page_objects/capital_assets/purap_transaction_page.rb
@@ -1,7 +1,7 @@
 class PurapTransactionPage < KFSBasePage
 
   element(:select) { |b| b.checkbox(name: 'purApDoc[0].purchasingAccountsPayableItemAsset[0].selectedValue') }
-  element(:split_qty) { |b| b.text_field(name: 'purApDoc[0].purchasingAccountsPayableItemAsset[0].splitQty') }
+  #element(:split_qty) { |b| b.text_field(name: 'purApDoc[0].purchasingAccountsPayableItemAsset[0].splitQty') }
 
   action(:view_doc) { |b| b.frm.link(href: /cabPurApLine.do/).click }
 
