d8dbac8aa0e26db59f9e47f2ff5a5cfe8a8b7cda
Nancy Kimble <nkk4@cornell.edu>
2014-06-09 10:24:27 -0400

    [KFSQA-988] Coding complete for successful test.

--- lib/kuality-kfs/page_objects/lookups/open_encumbrance_lookup_page.rb
+++ lib/kuality-kfs/page_objects/lookups/open_encumbrance_lookup_page.rb
@@ -13,6 +13,10 @@ class OpenEncumbranceLookupPage < Lookups
   element(:active_indicator_approved) { |b| b.frm.radio(id: 'dummyBusinessObject.pendingEntryOptionApproved') }
   element(:active_indicator_all) { |b| b.frm.radio(id: 'dummyBusinessObject.pendingEntryOptionAll') }
 
+  element(:including_pending_ledger_entry_no) { |b| b.frm.radio(id: 'dummyBusinessObject.pendingEntryOptionNo') }
+  element(:including_pending_ledger_entry_approved) { |b| b.frm.radio(id: 'dummyBusinessObject.pendingEntryOptionApproved') }
+  element(:including_pending_ledger_entry_all) { |b| b.frm.radio(id: 'dummyBusinessObject.pendingEntryOptionAll') }
+
   element(:including_zeroed_out_encumbrances_include) { |b| b.frm.radio(id: 'dummyBusinessObject.zeroEncumbranceOptionInclude') }
   element(:including_zeroed_out_encumbrances_exclude) { |b| b.frm.radio(id: 'dummyBusinessObject.zeroEncumbranceOptionExclude') }
 
