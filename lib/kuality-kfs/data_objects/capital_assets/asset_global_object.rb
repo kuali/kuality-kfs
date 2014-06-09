3050e92bf5f119ea31fac1652ba3ba467a4795e4
lshyu <lshyu@vivantech.com>
2014-06-09 09:44:16 -0400

    [KFSQA-994] added test CA build from AP transaction

--- lib/kuality-kfs/data_objects/capital_assets/asset_global_object.rb
+++ lib/kuality-kfs/data_objects/capital_assets/asset_global_object.rb
@@ -7,7 +7,7 @@ class AssetGlobalObject < KFSDataObject
     @browser = browser
 
     defaults = { description:    random_alphanums(20, 'AFT') }
-    set_options(defaults.merge(opts))
+    set_options(defaults.merge(get_aft_parameter_values_as_hash(ParameterConstants::DEFAULTS_FOR_ASSET_GLOBAL)).merge(opts))
   end
 
 
