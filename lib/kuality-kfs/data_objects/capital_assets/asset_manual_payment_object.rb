d1e38deb6291a695609598d65ee38828b20509e1
lshyu <lshyu@vivantech.com>
2014-06-10 16:34:15 -0400

    [KFSQA-997]  Do not need to open a new asset manual payment page.

--- lib/kuality-kfs/data_objects/capital_assets/asset_manual_payment_object.rb
+++ lib/kuality-kfs/data_objects/capital_assets/asset_manual_payment_object.rb
@@ -10,23 +10,25 @@ class AssetManualPaymentObject < KFSDataObject
   end
 
   def build
-    visit(MainPage).asset_manual_payment
-    on AssetManualPaymentPage do |page|
-      page.description.focus
-      page.alert.ok if page.alert.exists? # Because, y'know, sometimes it doesn't actually come up...
+    # TODO : Do not need to open a new page for QA-997.  Just needs an instance of this object.  Need to see if there is a better way to handle this
+    if !on(KFSBasePage).doc_title.include?('Asset Manual Payment')
+      visit(MainPage).asset_manual_payment
+      on AssetManualPaymentPage do |page|
+        page.description.focus
+        page.alert.ok if page.alert.exists? # Because, y'know, sometimes it doesn't actually come up...
 
-      fill_out page, :description
-      #TODO: Create accounting lines for Cap Assets with extra fields
-      # PO# REQ#, origin, doc.number, doc type, posted date
+        fill_out page, :description
+        #TODO: Create accounting lines for Cap Assets with extra fields
+        # PO# REQ#, origin, doc.number, doc type, posted date
 
 
-      #NOTE: Object Code looking for subtype from CAPITAL_OBJECT_SUB_TYPES of
-      #BK, AC, BE, CM, CF, CV, CT, BD, BF, CS, LA, IM, CL, UC, UF
+        #NOTE: Object Code looking for subtype from CAPITAL_OBJECT_SUB_TYPES of
+        #BK, AC, BE, CM, CF, CV, CT, BD, BF, CS, LA, IM, CL, UC, UF
 
-      #NOTE: while manually testing there was a yes/no page after submit with the data used
+        #NOTE: while manually testing there was a yes/no page after submit with the data used
 
+      end
     end
   end
-
 
 end #class
