77292a969efc4dd10bd6110d38ed4020e9587f9f
Nancy Kimble <nkk4@cornell.edu>
2014-07-08 15:15:59 -0400

    [KFSQA-970] Changes from the code review. Outstanding item is how to deal with documenting the specifics of obtaining the etup data.

--- lib/kuality-kfs/page_objects/labor_distribution/salary_expense_transfer_page.rb
+++ lib/kuality-kfs/page_objects/labor_distribution/salary_expense_transfer_page.rb
@@ -1,6 +1,6 @@
 class SalaryExpenseTransferPage < LaborDistributionPage
 
-  element(:empl_id) { |b| b.frm.text_field(name: 'document.emplid') }
+  element(:employee_id) { |b| b.frm.text_field(name: 'document.emplid') }
   action(:search_empl_id) { |b| b.frm.button(name: /^methodToCall.performLookup.\(!!org.kuali.rice.kim.api.identity.Person!!\)/m).click }
 
 end
