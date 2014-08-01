0a125d42c73faf1019339a21c75f6fc6a64b8498
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-08-01 12:54:06 -0400

    [KFSQA-856] Cleaned up some stuff and added an absorb method to PaymentRequestObject.

--- lib/kuality-kfs/data_objects/purap/payment_request_object.rb
+++ lib/kuality-kfs/data_objects/purap/payment_request_object.rb
@@ -34,6 +34,29 @@ class PaymentRequestObject < KFSDataObject
                         .merge(opts))
   end
 
+  def build
+    visit(MainPage).payment_request
+    initiate_request
+
+    on PaymentRequestPage do |page|
+      # Lots of data is prefilled on a PREQ.
+      absorb! :new
+      fill_out page, :vendor_address_1, :pay_date
+                      # == Not yet implemented in PaymentRequestPage: ==
+                      # :account_distribution_method, :payment_request_positive_approval_required,
+                      # :vendor_name, :vendor_number, :vendor_address_2, :attention,
+                      # :vendor_city, :vendor_state, :vendor_province, :vendor_postal_code, :vendor_country,
+                      # :purchase_order_number, :bank_code,
+                      # :invoice_date, :invoice_number, :vendor_invoice_amount,
+                      # :payment_method_code,
+                      # :freight_extended_cost, :freight_description,
+                      # :misc_extended_cost, :misc_description,
+                      # :sh_extended_cost, :sh_description,
+                      # :close_po
+    end
+
+  end
+
   # Fills out the Payment Request Initiation page that is the first step of creating a Payment Request
   def initiate_request
     on PaymentRequestInitiationPage do |page|
@@ -43,9 +66,7 @@ class PaymentRequestObject < KFSDataObject
       sleep 120 # This transition can take a long time. We'll sleep for 2 minutes, at least
     end
     on(YesOrNoPage).yes_if_possible # Sometimes it will ask for confirmation
-    on PaymentRequestPage do |page|
-      @number      = page.preq_id
-    end
+    @number = on(PaymentRequestPage).preq_id
   end
 
   # Fills out the Payment Request's tax tab. This should not be done during an edit,
@@ -78,6 +99,56 @@ class PaymentRequestObject < KFSDataObject
     update_options(opts)
   end
   alias_method :edit, :update
+
+  # Pull information from the page into the object. Note: this will not work for the PREQ initiation page!
+  # @param [Symbol] target The set of PREQ data to pull in
+  def absorb!(target=:new)
+    super
+    on(PaymentRequestPage).expand_all
+    case target
+      when :new; update_options(pull_new_preq_data)
+      when :old; update_options(pull_old_preq_data)
+      else
+        raise ArgumentError, "PaymentRequestObject#absorb! does not recognize the specified mode (#{target})."
+    end
+
+    update_line_objects_from_page!(target)
+  end
+
+  # @return [Hash] The return values of attributes for the old PREQ
+  def pull_old_preq_data
+    pulled_preq = Hash.new
+    on PaymentRequestPage do |prp|
+      pulled_preq = {
+        vendor_address_1: prp.result_vendor_address_1,
+        pay_date:         prp.pay_date.value.strip
+      }
+    end
+    pulled_preq.merge(pull_preq_extended_data(:old))
+  end
+
+  # @return [Hash] The return values of attributes for the new PREQ
+  def pull_new_preq_data
+    pulled_preq = Hash.new
+    on PaymentRequestPage do |prp|
+      pulled_preq = {
+        vendor_address_1: prp.vendor_address_1.value.strip,
+        pay_date:         prp.pay_date.value.strip
+      }
+    end
+    pulled_preq.merge(pull_preq_extended_data(:new))
+  end
+
+  # @return [Hash] The return values of extended attributes for the old PREQ
+  # @param [Symbol] target The set of PREQ data to pull in
+  def pull_preq_extended_data(target=:new)
+    Hash.new
+  end
+
+  def save
+    @number = on(PaymentRequestPage).preq_id
+    super
+  end
 
   def submit
     @number = on(PaymentRequestPage).preq_id
--- lib/kuality-kfs/page_objects/purap/payment_request_page.rb
+++ lib/kuality-kfs/page_objects/purap/payment_request_page.rb
@@ -3,6 +3,10 @@ class PaymentRequestPage < KFSBasePage
   element(:vendor_address_1) { |b| b.frm.text_field(name: 'document.vendorLine1Address') }
   element(:pay_date) { |b| b.frm.text_field(name: 'document.paymentRequestPayDate') }
 
+  element(:vendor_section) { |b| b.frm.table(class: 'datatable', summary: 'Vendor Section') }
+
+  value(:result_vendor_address_1) { |b| b.vendor_section.rows[3].tds[0].text.strip }
+
   # Tax Tab
   element(:income_class_code) { |b| b.frm.select(id: 'document.taxClassificationCode') }
   element(:federal_tax_pct) { |b| b.frm.text_field(id: 'document.taxFederalPercent') }
