291b675d6b27c977043a8d6d6ff1bbd1f0a6212b
Nancy Kimble <nkk4@cornell.edu>
2014-08-18 16:31:51 -0400

    [KFSQA-1012] Changes to implement this test.

--- lib/kuality-kfs/page_objects/labor_distribution/benefit_expense_transfer_page.rb
+++ lib/kuality-kfs/page_objects/labor_distribution/benefit_expense_transfer_page.rb
@@ -1,5 +1,10 @@
-class BenefitExpenseTransferPage < LaborDistributionPage
+class BenefitExpenseTransferPage < FinancialProcessingPage
 
+  action(:import_search) { |b| b.frm.button(title: 'search', name: /methodToCall.performBalanceInquiryLookup/m).click }
+  element(:fiscal_year) { |b| b.frm.text_field(name: 'universityFiscalYear') }
+  action(:search_fiscal_year) { |b| b.frm.button(title: 'Search Fiscal Year').click }
+
+  action(:copy_all_source_accounting_lines) { |l=0, b| b.frm.button(name: "methodToCall.copyAllAccountingLines").click }
   element(:chart_code) { |b| b.frm.text_field(name: 'chartOfAccountsCode') }
   element(:account_number) { |b| b.frm.text_field(name: 'accountNumber') }
   element(:sub_account_number) { |b| b.frm.text_field(name: 'subAccountNumber') }
