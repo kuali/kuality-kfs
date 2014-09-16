f8fd2778248ed10fdba2e8532f123e437247884e
lshyu <lshyu@vivantech.com>
2014-09-16 11:34:53 -0400

    [KFSQA-1053] moved step definition

--- features/step_definitions/financial_processing/financial_documents.rb
+++ features/step_definitions/financial_processing/financial_documents.rb
@@ -29,21 +29,3 @@ And /^I add a (From|To|Source|Target) Accounting Line to the (.*) document with 
 
 end
 
-
-And /^I lookup a Service Bill Processor as initiator and an associated account as source account$/ do
-  step 'I am logged in as a KR Technical Administrator'
-  visit(AdministrationPage).role
-  on RoleLookup do |lookup|
-    lookup.id.fit      '13'
-    lookup.search
-    lookup.edit_random # There can only be one!
-  end
-  # puts on(RolePage).frm.div(id: 'tab-Assignees-div').tables[2].rows.length
-  on RolePage do |page|
-    random_row = rand(2..page.member_table.rows.length - 1)
-    puts page.member_table[random_row][4].text,page.member_table[random_row][8].text
-    @sb_account = page.member_table[random_row][8].text
-    step "I am logged in as \"#{page.member_table[random_row][4].text}\""
-  end
-
-end
